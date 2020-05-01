f15ac53c49347fb6cd2147558666397eee146a41
Chloe Page <lovechloepuff@gmail.com>
2020-05-01 16:57:58 -0400

    adding in a ton of audio and a UI script

---
+++ AstralBeings/Assets/Scripts/RepeatQuestion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RepeatQuestion : MonoBehaviour
+{
+    AudioSource Question;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Question = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+   public void QuestionPlay()
+    {
+        Question.Play();
+        Debug.Log("clicked");
+    }
+}
