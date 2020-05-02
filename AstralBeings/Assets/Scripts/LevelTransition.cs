7c23ca5c980c360cc2f2d5a79e34dcc1cebcc154
Chang <hahnc589@newschool.edu>
2020-05-02 04:33:57 -0400

    chang

---
+++ AstralBeings/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public Animator transition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadNextLevel() {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
+    }
+    IEnumerator LoadLevel(int levelIndex) {
+        Debug.Log("hi");
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
