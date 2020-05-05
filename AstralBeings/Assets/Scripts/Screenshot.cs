1099d91562ed3cde733fcd4856a2c199e8d7c149
Chang <hahnc589@newschool.edu>
2020-05-05 10:27:39 -0400

    chang

--- AstralBeings/Assets/Scripts/Screenshot.cs
+++ AstralBeings/Assets/Scripts/Screenshot.cs
@@ -16,6 +16,10 @@ public class Screenshot : MonoBehaviour
     }
     private void OnPostRender()
     {
+        string defaultLocation = Application.dataPath + "/CameraSreenshot.png";
+        string myFolderLocation = "/storage/emulated/0/DCIM/Camera/Game";
+        string myFileName = "/CameraScreenshot.png";
+        string myLocation = myFolderLocation + myFileName;
         if (takeScreenshotOnNextFrame) {
             takeScreenshotOnNextFrame = false;
             RenderTexture renderTexture = myCamera.targetTexture;
@@ -23,13 +27,32 @@ public class Screenshot : MonoBehaviour
             Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
             Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
             renderResult.ReadPixels(rect, 0, 0);
+            
+            ////
+            if (!System.IO.Directory.Exists(myFolderLocation))
+            {
+                System.IO.Directory.CreateDirectory(myFolderLocation);
+            }
 
+            ////
+            ///
             byte[] byteArray = renderResult.EncodeToPNG();
-            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraSreenshot.png", byteArray);
+            System.IO.File.WriteAllBytes(myLocation, byteArray);
             Debug.Log("Saved CameraScreenshot.png");
 
             RenderTexture.ReleaseTemporary(renderTexture);
             myCamera.targetTexture = null;
+           
+            
+            //
+            System.IO.File.Move(defaultLocation, myLocation);
+           
+            AndroidJavaClass classPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject objActivity = classPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            AndroidJavaClass classUri = new AndroidJavaClass("android.net.Uri");
+            AndroidJavaObject objIntent = new AndroidJavaObject("android.content.Intent", new object[2] { "android.intent.action.MEDIA_MOUNTED", classUri.CallStatic<AndroidJavaObject>("parse", "file://" + myLocation) });
+            objActivity.Call("sendBroadcast", objIntent);
+            //
         }
     }
 
@@ -40,6 +63,7 @@ public class Screenshot : MonoBehaviour
 
     public static void TakeScreenshot_Static(int width, int height) {
         instance.TakeScreenshot(width, height);
+       
     }
 
 }
