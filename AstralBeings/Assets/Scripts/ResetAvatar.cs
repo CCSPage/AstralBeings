40917c39bb41b8808af7b90f4632fc0fca44a2c4
Chang <hahnc589@newschool.edu>
2020-05-05 13:35:26 -0400

    chang

---
+++ AstralBeings/Assets/Scripts/ResetAvatar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetAvatar : MonoBehaviour
+{
+    
+    //string sceneName = currentScene.name;
+
+
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene == SceneManager.GetSceneByName("Intro"))
+        {
+            if (transform.childCount > 0)
+            {
+                Destroy(transform.GetChild(0).gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            if (transform.childCount > 0)
+            {
+                Destroy(transform.GetChild(0).gameObject);
+            }
+        }
+    }
+}
